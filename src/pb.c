f79ed015a5ebe106c9df155b14877e2bac5d3694
ricardosantosaugusto <augusto.santos-pronet@paysmart.com.br>
2015-07-23 09:19:33 -0300

    fixed spelling error

--- src/pb.c
+++ src/pb.c
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
 {
     int balls[6];
     int count_balls = 0;
-    int favorite = 0;   // this should be a bool
+    int favourite = 0;   // this should be a bool
 
     for (int i=1; i<argc; i++)
     {
@@ -61,9 +61,9 @@ int main(int argc, char** argv)
 
         if ('-' == arg[0])
         {
-            if (0 == strcmp(arg, "-favorite"))
+            if (0 == strcmp(arg, "-favourite"))
             {
-                favorite = 1;
+                favourite = 1;
             }
             else
             {
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
         result = result * 2;
     }
 
-    if (favorite)
+    if (favourite)
     {
         result = result * 2;
     }
@@ -117,6 +117,6 @@ int main(int argc, char** argv)
     return 0;
 
 usage_error:
-    fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power_ball\n", argv[0]);
+    fprintf(stderr, "Usage: %s [-favourite] (5 white balls) power_ball\n", argv[0]);
     return -1;
 }
